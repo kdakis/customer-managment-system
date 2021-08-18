15db68c250e5db655f645365870150bdf7dd234f
kdakis <kazimakis@gmail.com>
2021-08-18 15:56:45 +0300

    postgresql veritabanı bağlandı

--- cms/settings.py
+++ cms/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cms_db',
+        'USER': 'kdakis',
+        'PASSWORD': '417383',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
